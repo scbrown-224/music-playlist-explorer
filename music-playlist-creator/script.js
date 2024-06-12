950833c1411625d9dd63f2a22cdd2824c26d2f48
Sydney Brown <sydney.brown@sydneyb-ltmd3u4.internal.salesforce.com>
2024-06-11 17:23:39 -0700

    playlist display

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,169 @@
+// Get the modal
+var modal = document.getElementById("myModal");
 
+function setupModal(buttonId) {
+
+  // Get the button that opens the modal
+  var btn = document.getElementById(buttonId);
+
+  // Get the <span> element that closes the modal
+  var span = modal.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+setupModal("card1");
+setupModal("card2");
+setupModal("card3");
+setupModal("card4");
+setupModal("card5");
+setupModal("card6");
+setupModal("card7");
+setupModal("card8");
+
+
+data.playlists.forEach(function(playlist){
+  let test = `<div class="playlist-cards" id="card1"> 
+                <img class="playlist-img" src="${playlist.playlist_art}">
+                <h3 class="playlist-title">${playlist.playlist_name}</h3>
+                <p class="artist-name">${playlist.playlist_creator}</p>
+                <div class="likes">
+                    <span>&#9825;</span>
+                </div> </div>
+
+                <!-- The Modal -->
+                <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                    <div class="modal-content">
+                        <span class="close">&times;</span>
+                            <div class="modal-header">
+                                <img id="img1" class="modal-img" src="${playlist.playlist_art}">
+                                <div class="header-text">
+                                    <h2 id="name1">${playlist.playlist_name}</h2>
+                                    <h3>${playlist.playlist_creator}</h3>
+                                </div>
+                            
+                            </div>
+                            <div class="modal-body">
+                                
+                            </div>
+                    </div>
+                </div>`;
+  
+  let all_playlists_div = document.getElementsByClassName("all-playlists");
+  all_playlists_div[0].innerHTML += test;
+  
+  
+  
+  
+  
+  
+  // console.log(playlist);
+
+
+
+  // let test = `<div class="playlist-cards" id="card1"> 
+  //               <img class="playlist-img" src="${playlist.playlist_art}">
+  //               <h3 class="playlist-title">${playlist.playlist_name}</h3>
+  //               <p class="artist-name">${playlist.playlist_creator}</p>
+  //               <div class="likes">
+  //                   <span>&#9825;</span>
+  //               </div> </div>`;
+
+  // let all_playlists_div = document.getElementsByClassName("all-playlists");
+  // all_playlists_div[0].innerHTML += test;
+
+  // console.log(all_playlists_div[0]);
+
+
+/*
+  `<div class="playlist-cards" id="card1"> 
+                <img class="playlist-img" src="/music-playlist-creator/assets/img/playlist.png">
+                <h3 class="playlist-title">Playlist Title</h3>
+                <p class="artist-name">${playlist.playlist_creator}</p>
+                <div class="likes">
+                    <span>&#9825;</span>
+                </div>
+
+                <!-- The Modal -->
+                <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                    <div class="modal-content">
+                        <span class="close">&times;</span>
+                            <div class="modal-header">
+                                <img id="img1" class="modal-img" src="/music-playlist-creator/assets/img/playlist.png">
+                                <div class="header-text">
+                                    <h2 id="name1">Playlist Title</h2>
+                                    <h3>artist name</h3>
+                                </div>
+                            
+                            </div>
+                            <div class="modal-body">
+                                <div class="modal-song-card">
+                                    <img id="song1" class="modal-song" src="/music-playlist-creator/assets/img/playlist.png">
+                                    <div class="modal-body-text">
+                                        <h3>song title</h3>
+                                        <p>artist name</p>
+                                        <p>album name</p>
+                                    </div>
+                                    <div class="modal-time">
+                                        <p>0:00</p>
+                                    </div>
+                                </div>
+
+                                <div class="modal-song-card">
+                                    <img id="song2" class="modal-song" src="/music-playlist-creator/assets/img/playlist.png">
+                                    <div class="modal-body-text">
+                                        <h3>song title</h3>
+                                        <p>artist name</p>
+                                        <p>album name</p>
+                                    </div>
+                                    <div class="modal-time">
+                                        <p>0:00</p>
+                                    </div>
+                                </div>
+
+                                <div class="modal-song-card">
+                                    <img id="song" class="modal-song" src="/music-playlist-creator/assets/img/playlist.png">
+                                    <div class="modal-body-text">
+                                        <h3>song title</h3>
+                                        <p>artist name</p>
+                                        <p>album name</p>
+                                    </div>
+                                    <div class="modal-time">
+                                        <p>0:00</p>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>`
+              
+
+    let playlist_card= document.createElement('div');
+    playlist_card.className = "playlist-cards";
+    playlist_card.id = `card${playlist.playlistID}`;
+      let playlist_img = document.createElement('img');
+      let playlist_name = document.createElement('h3');
+  let playlist_creator = document.createElement('p');
+  let playlist_likes = document.createElement('div');
+  let likes_img = document.createElement('span');
+*/
+
+});
